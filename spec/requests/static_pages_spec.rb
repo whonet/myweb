3666e6d775cc0ebcae4755df8a9a3660c5e0d164
whonet <whonet@163.com>
2014-06-15 17:01:49 +0800

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       expect(page).to have_content('标我吧')
     end
 
-    it "should have the title '首页'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("标我吧 | 首页")
+      expect(page).to have_title("标我吧")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| 首页')
     end
   end
 
