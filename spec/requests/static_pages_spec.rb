7b1d788c3e8a5af868d8c8da2277f096524c7020
whonet <whonet@163.com>
2014-06-17 16:00:13 +0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content '标我吧'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('标我吧')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("标我吧")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| 首页')
-    end
+    it { should have_content('标我吧') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| 首页') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content '帮助'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('帮助')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("标我吧 | 帮助")
-    end
+    it { should have_content('帮助') }
+    it { should have_title(full_title('帮助')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content '关于我们'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('关于我们')
-    end
-
-    it "should have the title '关于我们'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("标我吧 | 关于我们")
-    end
+    it { should have_content('关于我们') }
+    it { should have_title(full_title('关于我们')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content '联系我们'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('联系我们')
-    end
-
-    it "should have the title '联系我们'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("标我吧 | 联系我们")
-    end
+    it { should have_content('联系我们') }
+    it { should have_title(full_title('联系我们')) }
   end
 end
