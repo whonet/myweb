8e2538d7e8d7e13ca7bbb490417b6c1ffe4ff7b4
whonet <whonet@163.com>
2014-06-23 18:51:44 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def show 
+    @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params) # 健壮参数 原来是(paroms[:user]) 
+  	if @user.save
+  	  flash[:success] = "欢迎来到“标我吧”网！"
+      redirect_to @user
+  	else
+  	  render 'new'
+  	end
+  end
+
+  private 
+
+    def user_params # 健壮参数
+      params.require(:user).permit(:name, :email, :password, 
+      					:password_confirmation)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,44 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('注册') }
     it { should have_title(full_title('注册')) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "立即注册" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "账户名",         with: "Example User"
+        fill_in "电子邮件",        with: "user@example.com"
+        fill_in "密码",           with: "foobar"
+        fill_in "密码确认",        with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
